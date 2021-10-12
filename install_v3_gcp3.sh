7210fe1455ef8979e1c86e5cf10b8537099c56f5
Ronnie <62366124+not2007@users.noreply.github.com>
2021-10-12 17:09:47 +0800

    Update install_v3_gcp3.sh

--- install_v3_gcp3.sh
+++ install_v3_gcp3.sh
@@ -1,7 +1,5 @@
 mkdir ~/.streamrDocker01 ~/.streamrDocker02 ~/.streamrDocker03
 
-curl ip.sb
-
 sudo docker run -it -v $(cd ~/.streamrDocker01; pwd):/root/.streamr streamr/broker-node:testnet bin/config-wizard 
 sudo docker run -it -v $(cd ~/.streamrDocker02; pwd):/root/.streamr streamr/broker-node:testnet bin/config-wizard
 sudo docker run -it -v $(cd ~/.streamrDocker03; pwd):/root/.streamr streamr/broker-node:testnet bin/config-wizard 
@@ -13,3 +11,5 @@ sudo docker create --name st02 --restart=always -p 7270:7170 -p 7271:7171 -p 188
 sudo docker create --name st03 --restart=always -p 7370:7170 -p 7371:7171 -p 1883:1883 -v $(cd $HOME/.streamrDocker03; pwd):/root/.streamr streamr/broker-node:testnet
 
 sudo docker start st01 st02 st03
+
+curl ip.sb
